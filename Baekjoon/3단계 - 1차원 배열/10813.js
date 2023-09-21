ad91fd0208122972ac4ccb33603c6c758aa8ce82
zeroheat0930 <dongjunkorea@naver.com>
2023-09-21 17:52:03 +0900

    10813 공 바꾸기

---
+++ Baekjoon/3단계 - 1차원 배열/10813.js
@@ -0,0 +1,17 @@
+let fs = require("fs");
+let filePath = process.platform === "linux" ? "/dev/stdin" : "test.txt";
+
+let input = fs.readFileSync(filePath).toString().split("\n");
+let [N, M] = input[0].split(" ").map(Number);
+let arr = Array(N)
+  .fill(1)
+  .map((a, b) => a + b);
+
+for (let i = 1; i <= M; i++) {
+  let [x, y] = input[i].split(" ").map(Number);
+  let tmp = arr[x - 1];
+  arr[x - 1] = arr[y - 1];
+  arr[y - 1] = tmp;
+}
+
+console.log(arr.join(" "));
